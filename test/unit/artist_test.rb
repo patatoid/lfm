50df02d8d193e3dfe4d9f1e5382fa051bc193b31
pascal.knoth@gmx.com <knotito@CasseTeteChinois.home>
2012-10-17 19:14:17 +0200

    -dfs implementation (maybe try to do transactions next) -trying to implement undirected graphs, still work to do...

--- test/unit/artist_test.rb
+++ test/unit/artist_test.rb
@@ -3,21 +3,32 @@ require 'test_helper'
 class ArtistTest < ActiveSupport::TestCase
   fixtures :artists, :artist_edges
 
-  test "directed graphs edges" do
-    Artist.graph_options ? Artist.send(:acts_as_graph) : Artist.graph_options[:directed] = true
-     assert_equal [artists(:b),
-                  artists(:c),
-                  artists(:d),
-                  artists(:e)], 
-                artists(:a).children, 
-                "children relationship"
-    assert_equal [artists(:a),
-                  artists(:b),
-                  artists(:c),
-                  artists(:d)], 
-                artists(:e).parents, 
-                "parents relationship"
-    
+#  test "directed graphs edges" do
+#    Artist.respond_to?('graph_options') ? Artist.graph_options[:directed] = true : Artist.send(:acts_as_graph, :directed => true) 
+#    assert_equal [artists(:b),
+#      artists(:c),
+#      artists(:d),
+#      artists(:e)], 
+#      artists(:a).children, 
+#      "children relationship"
+#    assert_equal [artists(:a),
+#      artists(:b),
+#      artists(:c),
+#      artists(:d)], 
+#      artists(:e).parents, 
+#      "parents relationship"
+#
+#  end
+#
+  test "undirected graph edges" do
+    Artist.respond_to?('graph_options') ? Artist.graph_options[:directed] = false : Artist.send(:acts_as_graph, :directed => false) 
+    puts Artist.graph_options.inspect
+    assert_equal [artists(:b),
+      artists(:c),
+      artists(:d),
+      artists(:e)], 
+      artists(:a).neighbours, 
+      "neighbour relationship"
   end
 
   test "bad test" do

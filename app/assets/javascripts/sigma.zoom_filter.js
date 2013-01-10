f2be5f5f66aaac12cca8c653bddbaaad185740af
pascal.knoth@gmx.com <knotito@CasseTeteChinois.home>
2013-01-10 01:08:12 +0100

    zoom filter nearly finished TODO -fix zoom/move problem

--- app/assets/javascripts/sigma.zoom_filter.js
+++ app/assets/javascripts/sigma.zoom_filter.js
@@ -1,5 +1,15 @@
 sigma.zoom_filter = sigma.zoom_filter || {};
 
+//tool to clone functions in order to override draw method
+Function.prototype.clone = function() {
+	var self =this;
+	var temp = function temporary() { return self.apply(this, arguments); };
+	for (key in this) {
+		temp[key] = this[key];
+	}
+	return temp;
+};
+
 sigma.zoom_filter.zoom_filter = function(sigInst) {
 	sigma.classes.Cascade.call(this);
 	
@@ -14,10 +24,11 @@ sigma.zoom_filter.zoom_filter = function(sigInst) {
 	this.hidden_nodes = [];
 
 	this.isOnScreen = function(node){
-		return (node['displayX'] < sigInst._core.width) &&
-			(node['displayX'] > 0) &&
-			(node['displayY'] < sigInst._core.height) &&
-			(node['displayY'] > 0)
+		return (node['hidden']) ||
+			(node['displayX'] + node['displaySize'] < sigInst._core.width) &&
+			(node['displayX'] - node['displaySize'] > 0) &&
+			(node['displayY'] + node['displaySize'] < sigInst._core.height) &&
+			(node['displayY'] - node['displaySize'] > 0)
 	}
 
 	this.check_nodes = function() {
@@ -35,59 +46,88 @@ sigma.zoom_filter.zoom_filter = function(sigInst) {
 		})
 	}
 
-	this.display_offScreen_visible_nodes = function() {
+	this.display_hidden_offScreen_visible_nodes = function() {
 		document.getElementById('inScreen').innerHTML = "";
 		document.getElementById('outScreen').innerHTML = "";
 		document.getElementById('hidden').innerHTML = "";
-		self.visible_nodes.forEach(function(node) {document.getElementById('inScreen').innerHTML += node['label'] + " - "});
-		self.offScreen_nodes.forEach(function(node) {document.getElementById('outScreen').innerHTML += node['label'] + " - "});
-		self.hidden_nodes.forEach(function(node) {document.getElementById('hidden').innerHTML += node['label'] + " - "});
+		self.visible_nodes.forEach(function(node) {document.getElementById('inScreen').innerHTML += node['size'] + " - "});
+		self.offScreen_nodes.forEach(function(node) {document.getElementById('outScreen').innerHTML += node['size'] + " - "});
+		self.hidden_nodes.forEach(function(node) {document.getElementById('hidden').innerHTML += node['size'] + " - "});
 	}
 
 	this.filter = function() {
 		self.check_nodes();
 
-		if (self.visible_nodes.length > self.p.max_displayed_nodes){
-			self.visible_nodes.sort(function(a, b) {return a['weight'] - b['weight']});
+		self.visible_nodes.sort(function(a, b) {return b['size'] - a['size']});
+		self.hidden_nodes.sort(function(a, b) {return b['size'] - a['size']});
+		
+		while ((!self.visible_nodes[0] == null && !self.hidden_nodes[0] == null) && 
+				(self.visible_nodes[0]['size'] < self.hidden_nodes[0]['size'])) {
+			self.visible_nodes.push(self.hidden_nodes.shift());
+			self.visible_nodes.sort(function(a, b) {return b['size'] - a['size']});
+		}
+		
+		if (self.visible_nodes.length > self.p.max_displayed_nodes) {
 			self.visible_nodes.splice(self.p.max_displayed_nodes, self.visible_nodes.length).forEach(function(node) {
 				self.hidden_nodes.push(node)
 			})
 		}else{
-			self.hidden_nodes.sort(function(a, b) {return b['weight'] - a['weight']});
 			diff = self.p.max_displayed_nodes - self.visible_nodes.length
-			self.hidden_nodes.splice(0, self.hidden_nodes.length < diff ? self.hidden_nodes.length : diff).forEach(function(node) {
+			self.hidden_nodes.filter(function(node) {return self.isOnScreen(node)}).splice(0, self.hidden_nodes.length < diff ? self.hidden_nodes.length : diff).forEach(function(node) {
 				self.visible_nodes.push(node);
+				self.hidden_nodes.splice(self.hidden_nodes.indexOf(node), 1);
 			})
 		}
 
 		self.hidden_nodes.forEach(function(node) {
-			i = sigInst._core.graph.nodes.indexOf(node);
-			if (i) {sigInst._core.graph.nodes.splice(i, 1)}
+			//i = sigInst._core.graph.nodes.indexOf(node);
+			//if (i) {sigInst._core.graph.nodes.splice(i, 1)}
+			node['hidden'] = true;
 		});
 
 		self.visible_nodes.forEach(function(node) {
-			i = sigInst._core.graph.nodes.indexOf(node);
-			if (i) {sigInst._core.graph.nodes.push(node)}
+			//i = sigInst._core.graph.nodes.indexOf(node);
+			//if (i) {sigInst._core.graph.nodes.push(node)}
+			node['hidden'] = false;
 		});
 
-		self.display_offScreen_visible_nodes();
-		sigInst.draw();
+	//	self.display_hidden_offScreen_visible_nodes();
 	}
 
 	this.delete = function() {
 		self.hidden_nodes.forEach(function(node) {
-			sigInst._core.graph.nodes.push(node)
+			//sigInst._core.graph.nodes.push(node)
+			node['hidden'] = false;
 		});
-		sigInst.draw();
+
 		self.hidden_nodes = [];
+	}
+
+	this.temp_draw = sigInst._core.draw.clone();
+
+	this.stop_filtering = function() {
+		self.delete();
+		sigInst._core.draw = function(a,b,c,d) {
+			return self.temp_draw(a,b,c,d);	
 		}
+		sigInst.draw();
+	}
+
+	this.start_filtering = function() {
+		sigInst._core.draw = function(a,b,c,d) {
+			self.filter();
+			return self.temp_draw(a,b,c,d);	
+		}
+		sigInst.draw();
+	}
+
 //	sigInst._core.domRoot.addEventListener('mousewheel', function(e) {self.filter()}, false);
 	document.getElementById('button').addEventListener('click', function() {
 		if (document.getElementById('button').innerHTML == 'filter') {
-			self.filter();
+			self.start_filtering();
 			document.getElementById('button').innerHTML = 'unfilter';
 		}else{
-			self.delete();
+			self.stop_filtering();
 			document.getElementById('button').innerHTML = 'filter';
 		}
 	}, true);

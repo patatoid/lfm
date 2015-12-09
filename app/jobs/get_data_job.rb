8b662397549e3e5d608d1243d7de350ad377dffa
Patatoid <pascal@knoth.fr>
2015-12-09 13:18:43 +0100

    limit to get data to fix heap bug

--- app/jobs/get_data_job.rb
+++ app/jobs/get_data_job.rb
@@ -2,11 +2,7 @@ class GetDataJob < ActiveJob::Base
   queue_as :default
 
   def perform(*args)
-    unless Artist.all.any?
-      a = LFM::Artist.get_correction("Portishead")
-      a = Artist.create(:name => a.name, :mbid => a.mbid)
-    end
-    Artist.where(visited: nil).each do |a|
+    Artist.where(visited: nil).limit(100).each do |a|
       a.graph(10, 100)
     end
   end
